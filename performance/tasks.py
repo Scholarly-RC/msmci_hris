02c95145a6e88e3ec825e82d38544ab28d618f33
Charles-Andrew <barsubiacharles@gmail.com>
2024-08-29 20:45:52 +0800

    Utilized filelock to manage multiple conversion issue.

--- performance/tasks.py
+++ performance/tasks.py
@@ -2,10 +2,17 @@ import os
 import subprocess
 
 from django.conf import settings
+from filelock import FileLock
 
 
 def convert_document_to_pdf(uploaded_file_instance, file_name):
+    lock_file = "/tmp/pdf_conversion.lock"
+    lock = FileLock(lock_file, timeout=600)
+
     try:
+        lock.acquire()
+        print("Lock acquired.")
+
         document_path = uploaded_file_instance.resource.path
         document_extension = uploaded_file_instance.get_file_extension()
 
@@ -19,6 +26,8 @@ def convert_document_to_pdf(uploaded_file_instance, file_name):
             document_path,
         ]
 
+        print(f"Running command: {' '.join(command)}")
+
         subprocess.run(command, check=True, capture_output=True, text=True)
 
         output_pdf_path = document_path.replace(document_extension, ".pdf")
@@ -26,11 +35,22 @@ def convert_document_to_pdf(uploaded_file_instance, file_name):
         if os.path.exists(output_pdf_path):
             uploaded_file_instance.resource_pdf.name = output_pdf_path
             uploaded_file_instance.save()
+            print("PDF conversion successful.")
+        else:
+            print("PDF conversion failed: Output file not found.")
 
     except subprocess.CalledProcessError as e:
         print(f"Error during PDF conversion: {e.stderr}")
-        raise e
+        raise
 
     except Exception as error:
         print(f"An error occurred: {error}")
-        raise error
+        raise
+
+    finally:
+        lock.release()
+        print("Lock released.")
+
+        if os.path.exists(lock_file):
+            os.remove(lock_file)
+            print("Lock file removed.")
