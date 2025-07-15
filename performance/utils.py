62d5afcb1281303b626d20db75e4907cf82458ac
charles <barsubiacharles@gmail.com>
2025-07-15 13:26:26 +0800

    Exclude superuser in approvers

--- performance/utils.py
+++ performance/utils.py
@@ -253,8 +253,10 @@ def get_user_with_hr_role():
     UserDetailsModel = apps.get_model("core", "UserDetails")
 
     hr_role = UserDetailsModel.Role.HR.value
-    hr = UserModel.objects.select_related("userdetails").filter(
-        is_active=True, userdetails__role=hr_role
+    hr = (
+        UserModel.objects.select_related("userdetails")
+        .filter(is_active=True, userdetails__role=hr_role)
+        .exclude(is_superuser=True)
     )
     return hr
 
